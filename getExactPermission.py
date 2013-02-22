813f3b5a0416b98f00d1da40c88f8c07787d8fd2
Zhengyang Qu <ZhengyangQu2017@u.northwestern.edu>
2013-02-22 03:12:40 -0600

    Bugs fixed on handling 'NONE' and 'or '

--- getExactPermission.py
+++ getExactPermission.py
@@ -31,12 +31,14 @@ class ExactPermissionParser:
 						alllines = file_object.readlines()
 						for line in alllines:
 							line=line.strip()
+
 							if line.find('.apk')>=0 and (line.find('.apk')==(len(line)-4)):
 								OVERPRIVFDETECTED=0
 								PERMISSIONSDETECTED=0
 								packagename=line[:len(line)-4]
 								packagenum=packagenum+1
 								continue
+
 							if line.find("Stowaway does not think your application is overprivileged.")>=0: 
 								nonoverprivnum=nonoverprivnum+1
 								OVERPRIVFDETECTED=0
@@ -47,29 +49,68 @@ class ExactPermissionParser:
 								OVERPRIVFDETECTED=1
 								overprivnum=overprivnum+1
 								continue
+
 							if line=='Required Permissions': #line.find("Required Permissions")>=0:
 								OVERPRIVFDETECTED=0
 								continue
+
 							if line.find("Here, we list the permission-protected API calls, Content Providers, and Intents")>=0:
 								PERMISSIONSDETECTED=1
 								continue
+
 							if line=='Reflection Errors':
 								PERMISSIONSDETECTED=0
 								continue
+
 							if OVERPRIVFDETECTED==1 and line.find('Required Permissions')==-1:
 								overprivs.append(line)
 								continue
+
 							##handle the exact required permissions
 							if PERMISSIONSDETECTED==1 and line.find('Reflection Errors')==-1:
+								ContainOr=1
 								if line.find('[')>=0 and line.find(']')>=0:
 									temppermission=line[line.find('[')+1:line.find(']')]
-									if temppermission not in permissions:
-										permissions.append(temppermission)
+									if  temppermission.find(' or ') <0:
+										ContainOr=0
+										if (temppermission not in permissions) and temppermission!='NONE':
+											permissions.append(temppermission)
+										continue
+									first='XXXX'
+									remain=temppermission
+									while ContainOr==1:
+										first=remain[:remain.find(' or ')]
+										remain=remain[remain.find(' or ')+4:]
+										if (first not in permissions) and first!='NONE':
+											permissions.append(first)
+										if remain.find(' or ')<0:
+											ContainOr=0
+											break
+									if (remain not in permissions) and remain!='NONE':
+										permissions.append(remain)
+									continue
+
 								if line.find('Intent')==0 and line.find('requires')>=0:
 									temppermission=line[line.find('requires')+9:]
-									if temppermission not in permissions:
-										permissions.append(temppermission)	
-								continue
+									if  temppermission.find(' or ') <0:
+										ContainOr=0
+										if (temppermission not in permissions) and temppermission!='NONE':
+											permissions.append(temppermission)
+										continue
+									first='XXXX'
+									remain=temppermission
+									while ContainOr==1:
+										first=remain[:remain.find(' or ')]
+										remain=remain[remain.find(' or ')+4:]
+										if (first not in permissions) and first != 'NONE':
+											permissions.append(first)
+										if remain.find(' or ')<0:
+											ContainOr=0
+											break
+									if (remain not in permissions) and remain != 'NONE':
+										permissions.append(remain)
+									continue	
+
 							if line.find("Do you think the results here are wrong?")>=0:
 								maindict[packagename]=permissions
 								OVERPRIVFDETECTED=0
@@ -84,7 +125,8 @@ class ExactPermissionParser:
 
 		print json.dumps(maindict)
 		#for (k,v) in maindict.iteritems():
-			#print k + ":\n" + v +"\n"
+			#print k + ":" 
+			#print v
 		print "number of packages: " + "%s"%packagenum
 		print "number of non-overprivilage packages: " + "%s"%nonoverprivnum
 		print "number of overprivilage packages: " + "%s"%overprivnum
