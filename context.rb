c7d72e13f896077504ca2466e1c23fa93ae43e56
Jonathan Friedman <friedmanj98@gmail.com>
2013-02-27 13:33:34 -0600

    keyword

--- context.rb
+++ context.rb
@@ -1,11 +1,10 @@
 require 'rubygems'
 require 'alchemy_api'
 require 'json'
-require 'pry'
 
 AlchemyAPI.key = "bfc9f1c85f548d2ee9272e49ad3866f0e663cef1"
 
-file = File.open("json.txt")
+file = File.open("results/json.txt")
 c = file.read
 c = c.scan(/[[:print:]]/).join
 contents = JSON.parse(c)
@@ -13,8 +12,7 @@ contents = JSON.parse(c)
 results = []
 
 contents.each do |key, value|
-
-  result = AlchemyAPI.search(:concept_tagging, :text => value)
+  result = AlchemyAPI.search(:keyword_extraction, :text => value)
   # result = AlchemyAPI.search(:text_extraction, :text => value)
   # :term_extraction
   # :concept_tagging
