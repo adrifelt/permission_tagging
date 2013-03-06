be0b5f1c7d2c1348402ded421035a8925fbc426c
Jonathan Friedman <friedmanj98@gmail.com>
2013-03-05 19:30:59 -0600

    Feature: removed extra junk

--- context.rb
+++ context.rb
@@ -2,9 +2,13 @@ require 'rubygems'
 require 'alchemy_api'
 require 'json'
 require 'pry'
-require 'pry-exception_explorer'
 
-AlchemyAPI.key = "7aead646c8e2af56ce7477054f5b9405c22ffe2d"
+AlchemyAPI.key =
+['bfc9f1c85f548d2ee9272e49ad3866f0e663cef1',
+'4315f947e2ea43c6f2560f048ed25ac585efd94a',
+'d16d4ef723dce3523f2a85d108a5e903fd0c70c9',
+'7aead646c8e2af56ce7477054f5b9405c22ffe2d'].sample(1).first
+
 # extra keys
 # 'bfc9f1c85f548d2ee9272e49ad3866f0e663cef1'
 # '4315f947e2ea43c6f2560f048ed25ac585efd94a'
@@ -19,23 +23,17 @@ contents = JSON.parse(c)
 results = []
 
 contents.each do |key, value|
+  puts key + ".apk\n"
   values = []
   result = AlchemyAPI.search(:keyword_extraction, :text => value)
   if not result.nil?
     result.each do |r|
-      if r["relevance"].to_f > 0.8
-        values << r
-      end
-    end
-    category = nil
-    category = AlchemyAPI.search(:concept_tagging, :text => values.first["text"] + "," + values.last["text"] )
-    if not category.nil?
-      results << [key, values.to_json, category.to_json]
-    else
-      results << [key, values.to_json, nil]
+        puts r["text"] + "\n"
+        values << r["text"]
     end
   end
+  puts "\n"
 end
 
 
-puts results.to_json
+# puts values.to_json
