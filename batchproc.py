c8cd876bf23a77ac95c22c2652bcf50e78e5b437
Zhengyang Qu <ZhengyangQu2017@u.northwestern.edu>
2013-03-23 23:32:25 -0500

    batch process the .apk files using Stowaway

--- batchproc.py
+++ batchproc.py
@@ -24,9 +24,23 @@ class Depack:
         filepath=os.path.join(root, f)
 	if filepath.find('.apk')>=0:
 		name=os.path.basename(filepath)
-		os.chdir('/home/zyqu/Stowaway-1.2.4')
-		command='./stowaway.sh ~/apps/'+name+' ~/res/'+name+' 2>errors'
+		os.chdir('/home/peng/Stowaway-1.2.4')
+		command='./stowaway.sh ~/apps/'+name+' ~/res/'+name+' 1>temp'
         	os.system(command)
+		command='rm -r /home/peng/res/'+name+'/baksmali'
+            	os.system(command)
+        	command='rm -r /home/peng/res/'+name+'/dedex'
+            	os.system(command)
+        	command='rm -r /home/peng/res/'+name+'/IntentResults'
+            	os.system(command)
+        	command='rm -r /home/peng/res/'+name+'/log'
+            	os.system(command)
+        	command='rm -r /home/peng/res/'+name+'/ProviderResults'
+            	os.system(command)
+        	command='rm -r /home/peng/res/'+name+'/ReflectionResults'
+            	os.system(command)
+        	command='rm -r /home/peng/res/'+name+'/unzip'
+            	os.system(command)
 		count=count+1
 		print count
 
@@ -35,5 +49,5 @@ class Depack:
 
 
 if __name__=="__main__":
-  dep=Depack("/home/zyqu/apps/","/home/zyqu/res/")
+  dep=Depack("/home/peng/apps/","/home/peng/res/")
   dep.depackage()
